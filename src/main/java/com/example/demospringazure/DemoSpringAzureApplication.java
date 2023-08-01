a2a51c58cabfb9a53612bc66b56f989f69b89e72
Carlos <celiberato1@outlook.com>
2023-08-01 13:35:15 +0000

    first commit

---
+++ src/main/java/com/example/demospringazure/DemoSpringAzureApplication.java
@@ -0,0 +1,13 @@
+package com.example.demospringazure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringAzureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demospringazure/DemoSpringAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demospringazure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringAzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
