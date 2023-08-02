e08c941c10346e1d1bc4d6baccd760adea55a38d
celiberato <celiberato@hotmail.com>
2023-08-02 12:52:13 +0000

    Update HelloController.java

--- src/main/java/com/example/demospringazure/controller/HelloController.java
+++ src/main/java/com/example/demospringazure/controller/HelloController.java
@@ -23,7 +23,7 @@ public class HelloController {
 
 	@GetMapping("/hello")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		return "<stroing>Opa!! deploy efetuado com sucesso na AZURE</strog>";
 	}
 
 }
